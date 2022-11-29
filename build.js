500361a10c1f3215c65531a8adcb68255d3266c9
Subhajit Sahu <wolfram77@gmail.com>
2022-11-29 19:25:40 +0530

    :bug: update build setup

--- build.js
+++ build.js
@@ -1,7 +1,42 @@
-const cp = require('child_process');
+const cp    = require('child_process');
+const build = require('extra-build');
+
+const owner  = 'nodef';
 
 
-function makeExec() {
-  cp.execSync(`chmod +x index.sh`);
+// Publish a root package to NPM, GitHub.
+function publishRootPackage(ds, ver, typ) {
+  var _package = build.readDocument('package.json');
+  var _readme  = build.readDocument('README.md');
+  var m  = build.readMetadata('.');
+  m.version  = ver;
+  build.writeMetadata('.', m);
+  build.publish('.');
+  try { build.publishGithub('.', owner); }
+  catch {}
+  build.writeDocument(_package);
+  build.writeDocument(_readme);
 }
-makeExec();
+
+
+// Publish root packages to NPM, GitHub.
+function publishRootPackages(ds, ver) {
+  cp.execSync(`chmod +x index.sh`);
+  publishRootPackage(ds, ver, '');
+}
+
+
+// Pushish root, sub packages to NPM, GitHub.
+function publishPackages(ds) {
+  var m   = build.readMetadata('.');
+  var ver = build.nextUnpublishedVersion(m.name, m.version);
+  publishRootPackages(ds, ver);
+}
+
+
+// Finally.
+function main(a) {
+  if (a[2]==='publish-packages') publishPackages([]);
+  else bundleScript([]);
+}
+main(process.argv);
